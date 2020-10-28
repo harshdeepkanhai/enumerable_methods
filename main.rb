2330dc6015b9bfe526cbce386484bcdf1054d82a
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-10-28 15:03:14 -0600

    Add my_select method

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ module Enumerable
       yield(self[i])
       i += 1
     end
+    self
   end
 
   def my_each_with_index
@@ -13,5 +14,16 @@ module Enumerable
       yield(self[i], i)
       i += 1
     end
+    self
+  end
+
+  def my_select
+    arr = []
+    i = 0
+    while i < length
+      arr.push(self[i]) if yield(self[i])
+      i += 1
+    end
+    arr
   end
 end
